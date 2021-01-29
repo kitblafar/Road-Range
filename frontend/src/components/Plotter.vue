2ac172b8d84e52ab3904c09a99eaff6e9ec8b6b8
Kitty Farren <farren.kitty@gmail.com>
2021-01-29 18:27:25 +0000

    Plotted Live Data
    well done me

--- frontend/src/components/Plotter.vue
+++ frontend/src/components/Plotter.vue
@@ -4,13 +4,48 @@
 
 <script>
     import Chart from 'chart.js';
-    import arduinoData from '../Data.js';
+    // Create WebSocket connection.
+    const socket = new WebSocket('ws://localhost:1999');
+    const startTime = Date.now();
+
+    let arduinoData = {
+        type: 'line',
+        data: {
+            labels: [], //put time in here
+            datasets: [{
+                data: [], //put arduino data in here
+                label: "Arduino-Data",
+            },
+            ],
+        },
+
+        options: {
+            responsive: true,
+            lineTension: 1,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        padding: 25,
+                    }
+                }]
+            }
+        },
+    };
+
     export default {
+
         name: "Plotter",
         mounted() {
-            console.log(arduinoData);
-            this.createChart('sensor-chart', arduinoData);
+            let Chart1=this.createChart('sensor-chart', arduinoData);
             console.log("chart created");
+            // Listen for messages
+            socket.addEventListener('message', (event) => {
+                console.log('Message from server ', event.data);
+                //add data to the arduinodata.data array and the time to the label array
+                let time = Date.now() - startTime;
+                this.addData(Chart1, time, event.data);
+            });
         },
         methods: {
             data() {
@@ -25,8 +60,15 @@
                     data: chartData.data,
                     options: chartData.options,
                 });
-                console.log(myChart);
+                return(myChart); //make the chart accessible everywhere
             },
+            addData(chart, label, data) {
+                chart.data.labels.push(label);
+                chart.data.datasets.forEach((dataset) => {
+                    dataset.data.push(data);
+                });
+                chart.update();
+            }
         },
 
     }
